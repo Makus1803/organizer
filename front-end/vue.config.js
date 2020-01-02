0292bb766aadf04ad46ed7258b3111c336a51adb
Mateusz <mateusz.glocki@gmail.com>
2020-01-02 08:53:27 +0100

    dodanie mapowań w back-end

---
+++ front-end/vue.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/api/*': {
+        target: 'http://localhost:8080'
+      }
+    },
+    configureWebpack: {
+      entry: {
+        app: './src/main.js',
+        style: [
+          'bootstrap/dis/css/bootstrap.min.css'
+        ]
+      }
+    }
+  }
+}
+module.exports = {
+  devServer: {
+    clientLogLevel: 'info'
+  }
+};
