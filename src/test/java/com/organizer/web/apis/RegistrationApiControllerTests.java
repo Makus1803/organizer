569d13bfd26c0a859a0f5ed8fb349a526d313cf6
Mateusz <mateusz.glocki@gmail.com>
2020-01-07 14:16:36 +0100

    dodanie spring security

--- src/test/java/com/organizer/web/apis/RegistrationApiControllerTests.java
+++ src/test/java/com/organizer/web/apis/RegistrationApiControllerTests.java
@@ -1,5 +1,6 @@
 package com.organizer.web.apis;
 
+import com.organizer.config.SecurityConfiguration;
 import com.organizer.domain.application.UserService;
 import com.organizer.domain.model.user.EmailAddressExistsException;
 import com.organizer.domain.model.user.UsernameExistsException;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -21,7 +24,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(RegistrationApiController.class)
+@ContextConfiguration(classes = {SecurityConfiguration.class, RegistrationApiController.class})
+@WebMvcTest()
 public class RegistrationApiControllerTests {
 
     @Autowired
