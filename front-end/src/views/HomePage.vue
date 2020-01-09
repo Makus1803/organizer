ae4022151ecbef7f6fac66d62a5397360b22d179
Mateusz <mateusz.glocki@gmail.com>
2020-01-09 16:02:58 +0100

    implementacja testów e2e

--- front-end/src/views/HomePage.vue
+++ front-end/src/views/HomePage.vue
@@ -1,13 +1,9 @@
 <template>
-
+  <div><h1 class="page-title">Home Page</h1></div>
 </template>
 
 <script>
-    export default {
-        name: "HomePage"
-    }
+export default {
+  name: 'HomePage'
+}
 </script>
-
-<style scoped>
-
-</style>
